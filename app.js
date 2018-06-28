b86611d783423fc96801bb52e61d318f211b0315
Kiril Yurovnik <kyurovnik@gmail.com>
2018-06-29 01:34:22 +0300

    Added test route

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const testRouter = require('./routes/test');
+
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use('/test', testRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
